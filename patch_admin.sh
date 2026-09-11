b40dd619a683d6416e9be23589d6b807fb95a46e
guci-sketch <gucimas01@gmail.com>
2026-09-11 10:31:33 +0700

    update patch

--- patch_admin.sh
+++
@@ -1,1 +0,0 @@
-sed -i 's/import {  LocationMap } from '\''\.\.\/maps\/LocationMap'\'';/import {  LocationMap } from '\''.\/maps\/LocationMap'\'';\nimport { AllProjectsMap } from '\''..\/maps\/AllProjectsMap'\'';/' src/components/admin/AdminDashboard.tsx
--- patch_photo_review_label.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/components/admin/AdminDashboard.tsx', 'utf8');
-
-code = code.replace(
-  "Galeri Bukti Foto (",
-  "Tinjauan Bukti Foto (Photo Review) - ("
-);
-
-fs.writeFileSync('src/components/admin/AdminDashboard.tsx', code);
